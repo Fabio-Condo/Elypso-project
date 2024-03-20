8459cb8c370afa71ef121762044c15a67cb952b7
Fábio Condo <fabioenosse59@gmail.com>
2024-03-20 10:51:17 +0200

    update controller

--- src/main/java/org/elypso/controller/ElypsoController.java
+++ src/main/java/org/elypso/controller/ElypsoController.java
@@ -19,6 +19,8 @@ import java.io.IOException;
 @RequestMapping(path = {"/elypso"})
 public class ElypsoController {
 
+    private final String sessao = "JOB000001"; // Usar esta sessao para testar os endpoints/sequencia dos endpoints
+
     ElypsoService elypsoService;
 
     public ElypsoController(ElypsoService elypsoService) {
@@ -41,35 +43,35 @@ public class ElypsoController {
         return ResponseEntity.status(HttpStatus.OK).body(elypsoService.iniciarSequencia(impressora));
     }
 
-    @GetMapping("/inicializarProcessoImpressao")
-    public ResponseEntity<PrinterCenterResponse> inicializarProcessoImpressao() throws IOException {
-        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.inicializarProcessoImpressao("Evolis Elypso", "JOB000001"));
+    @GetMapping("/inicializarProcessoImpressao/{impressora}")
+    public ResponseEntity<PrinterCenterResponse> inicializarProcessoImpressao(@PathVariable("impressora") String impressora) throws IOException {
+        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.inicializarProcessoImpressao(impressora, sessao));
     }
 
-    @GetMapping("/configurarProcessoImpressao")
-    public ResponseEntity<PrinterCenterResponse> configurarProcessoImpressao() throws IOException {
-        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.configurarProcessoImpressao(Fita.RM_KO, "JOB000001"));
+    @GetMapping("/configurarProcessoImpressao/{fita}")
+    public ResponseEntity<PrinterCenterResponse> configurarProcessoImpressao(@PathVariable("fita") Fita fita) throws IOException {
+        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.configurarProcessoImpressao(fita, sessao));
     }
 
     @GetMapping("/definirBitmapImpressaoFrontal")
-    public ResponseEntity<PrinterCenterResponse> definirBitmapImpressaoFrontal() throws IOException, NomeOuNumeroVazioException, FileNotFoundException {
-        Pedido pedido = new Pedido("NOME TESTE", "NUMERO CLIENTE", "NUMERO APOLICE", "Evolis Elypso", Fita.RM_KO, Lado.FRENTE_VERSO, "JOB000001");
+    public ResponseEntity<PrinterCenterResponse> definirBitmapImpressaoFrontal(@RequestBody Pedido pedido) throws IOException, NomeOuNumeroVazioException, FileNotFoundException {
+        pedido.setSessao(sessao); // Usar a sessao criada
         return ResponseEntity.status(HttpStatus.OK).body(elypsoService.definirBitmapImpressaoFrontal(pedido));
     }
 
     @GetMapping("/definirBitmapImpressaoTrazeiro")
     public ResponseEntity<PrinterCenterResponse> definirBitmapImpressaoTrazeiro() throws IOException, FileNotFoundException {
-        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.definirBitmapImpressaoTrazeiro("JOB000001"));
+        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.definirBitmapImpressaoTrazeiro(sessao));
     }
 
     @GetMapping("/realizarImpressao")
     public ResponseEntity<PrinterCenterResponse> realizarImpressao() throws IOException {
-        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.realizarImpressao("JOB000001"));
+        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.realizarImpressao(sessao));
     }
 
     @GetMapping("/finalizarImpressao")
     public ResponseEntity<PrinterCenterResponse> finalizarImpressao() throws IOException {
-        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.finalizarImpressao("JOB000001"));
+        return ResponseEntity.status(HttpStatus.OK).body(elypsoService.finalizarImpressao(sessao));
     }
 
     @GetMapping("/finalizarSequencia/{impressora}")
